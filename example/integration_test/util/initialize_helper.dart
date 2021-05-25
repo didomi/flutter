8156ac24f0ddc5be43e6106e457697e00732ce08
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-05-25 17:10:02 +0200

    fix initialize helper by using Future

--- example/integration_test/util/initialize_helper.dart
+++ example/integration_test/util/initialize_helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 class InitializeHelper {
-  static void initialize(WidgetTester tester, Finder finder) async {
+  static Future initialize(WidgetTester tester, Finder finder) async {
     await tester.tap(finder);
     await tester.pumpAndSettle();
 
