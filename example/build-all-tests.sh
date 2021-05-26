6903390e359e2b636241ba7955d5432a0b0fd7b3
Felipe Saez <felipe.saez.victoriano@gmail.com>
2021-05-26 09:37:34 +0200

    941-integration-tests-ios: updated Xcode project to allow device testing on Firebase

---
+++ example/build-all-tests.sh
@@ -0,0 +1,10 @@
+# Script used to iterate through all the dart test files contained in the integration_test directory and execute ./build-test.sh for each one of them.
+
+TEST_FILE_PATHS=`ls integration_test/*_test.dart`
+
+for TEST_FILE_PATH in $TEST_FILE_PATHS
+do
+  TEST_FILE=${TEST_FILE_PATH#"integration_test/"}
+  echo "Creating tests for $TEST_FILE"
+  ./build-test.sh $TEST_FILE
+done
