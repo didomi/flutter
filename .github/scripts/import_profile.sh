da56f3039016ba4a2360175a85fa9e1112962224
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-06-04 13:29:43 +0200

    custom flutter provisioning file #5

--- .github/scripts/import_profile.sh
+++ .github/scripts/import_profile.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/FlutterSample.mobileprovision
