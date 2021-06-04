fccf28a20035dbceeb4834db19bc50a32cf33dbe
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-06-04 14:29:50 +0200

    custom flutter provisioning file #11

--- .github/scripts/import_profile.sh
+++ .github/scripts/import_profile.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-uuid=$($(uuidgen) | tr '[:upper:]' '[:lower:]')
+uuid=$(echo $(uuidgen) | tr '[:upper:]' '[:lower:]')
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/"$uuid.mobileprovision"
