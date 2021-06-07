2814b83a139c2b8355ceb74e605cb1984b225b63
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-06-07 17:45:09 +0200

    remove unused scripts

--- .github/scripts/import_profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-uuid=$(echo $(uuidgen) | tr '[:upper:]' '[:lower:]')
-
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/"$uuid.mobileprovision"
