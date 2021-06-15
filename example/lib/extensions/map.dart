b580cfbc6679e7277df4a84c8ca64ec3dcd68730
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-06-15 14:30:35 +0200

    rename arguments for better understanding

--- example/lib/extensions/map.dart
+++ example/lib/extensions/map.dart
@@ -3,5 +3,5 @@ import 'dart:collection';
 /// Map extension.
 extension MapSorting on Map {
   /// Extension method used to sort map by keys.
-  SplayTreeMap sortByKey() => SplayTreeMap<String, dynamic>.from(this, (a, b) => this[a]!.compareTo(this[b]!));
+  SplayTreeMap sortByKey() => SplayTreeMap<String, dynamic>.from(this, (key1, key2) => this[key1]!.compareTo(this[key2]!));
 }
