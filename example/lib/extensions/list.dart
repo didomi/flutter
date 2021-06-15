34ceead2861bae3175bb4ee012f7403438a4c885
Nicolas CHAIX <nicolas.chaix@didomi.io>
2021-06-15 14:36:48 +0200

    cleanup list extensions

--- example/lib/extensions/list.dart
+++ example/lib/extensions/list.dart
@@ -1,18 +1,20 @@
 import 'package:didomi_sdk/entities/purpose.dart';
 import 'package:didomi_sdk/entities/vendor.dart';
 
+const _separator = ", ";
+
 /// List extension.
 extension ListPrettyPrint on List<String> {
-  /// Extension method used to print content.
-  String pretty() => this.join(", ");
+  /// Extension method used to return readable content.
+  String pretty() => this.join(_separator);
 }
 
 extension VendorListPrettyPrint on List<Vendor> {
-  /// Extension method used to print Vendor content.
-  String pretty() => this.map((vendor) => vendor.name).join(", ");
+  /// Extension method used to return readable Vendor content.
+  String pretty() => this.map((vendor) => vendor.name).join(_separator);
 }
 
 extension PurposeListPrettyPrint on List<Purpose> {
-  /// Extension method used to print Purpose content.
-  String pretty() => this.map((purpose) => purpose.name).join(", ");
+  /// Extension method used to return readable Purpose content.
+  String pretty() => this.map((purpose) => purpose.name).join(_separator);
 }
