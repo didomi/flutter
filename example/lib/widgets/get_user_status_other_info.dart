636c21c1368181d949bf05e0693998c5f73845f1
Philemon Merlet <philemon.merlet@didomi.io>
2022-01-28 09:41:46 +0100

    Small cleanup

--- example/lib/widgets/get_user_status_other_info.dart
+++ example/lib/widgets/get_user_status_other_info.dart
@@ -4,7 +4,6 @@ import 'package:didomi_sdk/didomi_sdk.dart';
 import 'package:didomi_sdk/entities/user_status.dart';
 import 'package:didomi_sdk_example/widgets/base_sample_widget_state.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as developer;
 
 /// Widget to call DidomiSdk.getUserStatus and display additional info from result
 class GetUserStatusOtherInfo extends StatefulWidget {
@@ -22,7 +21,6 @@ class _GetUserStatusOtherInfo extends BaseSampleWidgetState<GetUserStatusOtherIn
   @override
   Future<String> callDidomiPlugin() async {
     final UserStatus result = await DidomiSdk.userStatus;
-    developer.log("!!!!!! Consent Status : ${result.consentString}");
     return "Other info in status: \n"
         "- User id ${result.userId}\n"
         "- Created ${result.created}, updated ${result.updated}\n"
