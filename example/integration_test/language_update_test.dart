0ee8380b49ebe9b7c6e47f5c00124d5c0f2f249c
Nicolas CHAIX <nicolas.chaix@didomi.io>
2022-07-22 16:18:09 +0200

    use init helper

--- example/integration_test/language_update_test.dart
+++ example/integration_test/language_update_test.dart
@@ -7,6 +7,7 @@ import 'package:integration_test/integration_test.dart';
 
 import 'util/assertion_helper.dart';
 import 'util/constants.dart';
+import 'util/initialize_helper.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -64,10 +65,7 @@ void main() {
       assert(isReady == false);
 
       // Initialize SDK
-      await tester.tap(initializeBtnFinder);
-      await tester.pumpAndSettle();
-
-      await Future.delayed(Duration(seconds: 4));
+      await InitializeHelper.initialize(tester, initializeBtnFinder);
 
       assert(isError == false);
       assert(isReady == true);
