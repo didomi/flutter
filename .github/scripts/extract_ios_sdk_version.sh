567665ba4d48698944b2ee7aa9ea6c337a5dd05e
Nicolas CHAIX <nicolas.chaix@didomi.io>
2022-09-16 13:14:29 +0200

    fix error log message

--- .github/scripts/extract_ios_sdk_version.sh
+++ .github/scripts/extract_ios_sdk_version.sh
@@ -7,7 +7,7 @@
 
 version=$(cat ios/didomi_sdk.podspec | sed -n "s|.*s.dependency[ ]*'Didomi-XCFramework', '\([^']*\)'.*|\1|p")
 if [[ ! $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-  echo "Error while getting flutter version"
+  echo "Error while getting iOS version"
   exit 1
 fi
 
