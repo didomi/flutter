74b66c1974391964f67d7430ea108def00e33f10
Nicolas CHAIX <nicolas.chaix@didomi.io>
2023-03-01 11:03:25 +0100

    cleanup increment function

--- .github/scripts/update_bridge_version.sh
+++ .github/scripts/update_bridge_version.sh
@@ -28,14 +28,14 @@ shopt -u nocasematch
 #   - increment number: `0` (major) | `1` (minor) | `2` (patch)
 increment_version() {
   local delimiter=.
-  local array=($(echo "$1" | tr $delimiter '\n'))
+  local array=("$(echo "$1" | tr $delimiter '\n')")
   array[$2]=$((array[$2] + 1))
-  if [ $2 -lt 2 ]; then array[2]=0; fi
-  if [ $2 -lt 1 ]; then array[1]=0; fi
-  echo $(
+  if [ "$2" -lt 2 ]; then array[2]=0; fi
+  if [ "$2" -lt 1 ]; then array[1]=0; fi
+  echo "$(
     local IFS=$delimiter
     echo "${array[*]}"
-  )
+  )"
 }
 
 # Get Flutter version
