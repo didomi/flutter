81da118c3ceb38dfaa45f4b8b59ccd276748b043
Philemon Merlet <philemon.merlet@didomi.io>
2024-02-16 17:37:06 +0100

    Fix unused imports

--- example/lib/testapps/sample_for_vendor_tests.dart
+++ example/lib/testapps/sample_for_vendor_tests.dart
@@ -2,8 +2,6 @@ import 'package:didomi_sdk_example/widgets/get_required_vendor_ids.dart';
 import 'package:didomi_sdk_example/widgets/get_required_vendors.dart';
 import 'package:didomi_sdk_example/widgets/get_vendor.dart';
 import 'package:didomi_sdk_example/widgets/initialize_small.dart';
-import 'package:didomi_sdk_example/widgets/set_user_agree_to_all.dart';
-import 'package:didomi_sdk_example/widgets/set_user_disagree_to_all.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(SampleForVendorTestsApp());
