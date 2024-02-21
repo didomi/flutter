adff859881cd24ad05037bbd9e0bd58f268cc5b1
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2024-02-21 11:32:40 +0100

    CMP-4509 - SPI parameter for `showPreferences` function (#102)
    * refactor parameter for `showPreferences` function
    
    * handle SPI parameter for iOS implementation of `showPreferences`
    
    * add SPI radio item for `showPreferences`

--- lib/preferences_view.dart
+++ lib/preferences_view.dart
@@ -3,6 +3,24 @@ enum PreferencesView {
   /// Main preferences screen
   purposes,
 
+  /// Sensitive Personal Information preferences screen
+  sensitivePersonalInformation,
+
   /// Vendors preferences screen
   vendors
+}
+
+/// Extension to get the name of the enum
+extension PreferencesViewExtension on PreferencesView {
+  String get name {
+    switch (this) {
+      case PreferencesView.sensitivePersonalInformation:
+        return "sensitive-personal-information";
+      case PreferencesView.vendors:
+        return "vendors";
+      default:
+        // purposes is the default value
+        return "purposes";
+    }
+  }
 }
