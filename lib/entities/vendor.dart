6b09865bc077ca63fbf8e718d85d970a84ef2964
Philemon Merlet <philemon.merlet@didomi.io>
2024-02-21 18:19:32 +0100

    Fix vendor class

--- lib/entities/vendor.dart
+++ lib/entities/vendor.dart
@@ -7,19 +7,21 @@ class Vendor implements Comparable<Vendor> {
   // URL to the privacy policy of the vendor.
   String? policyUrl;
   // Namespaces of the vendor (IAB, didomi or custom) and their corresponding IDs.
-  Map<String, String>? namespaces;
+  Namespaces? namespaces;
   // Purpose IDs that the vendor is operating under the consent legal basis.
   List<String> purposeIds;
   // Purpose IDs that the vendor is operating under the legitimate interest legal basis.
   List<String> legIntPurposeIds;
   // Set with IDs that represent features.
   List<String> featureIds;
-  // Set with IDs that represent flexible purposes. TCFv2 property.
+  // Set with IDs that represent flexible purposes. Introduced in IAB TCF v2.
   List<String> flexiblePurposeIds;
-  // Set with IDs that represent Special Purposes. TCFv2 property.
+  // Set with IDs that represent Special Purposes. Introduced in IAB TCF v2.
   List<String> specialPurposeIds;
-  // Set with IDs that represent Special Features. TCFv2 property.
+  // Set with IDs that represent Special Features. Introduced in IAB TCF v2.
   List<String> specialFeatureIds;
+  // Privacy policy and LI disclaimer urls. Introduced in IAB TCF v2.2.
+  List<Url>? urls;
 
   Vendor(
       this.id,
@@ -30,7 +32,8 @@ class Vendor implements Comparable<Vendor> {
       this.featureIds,
       this.flexiblePurposeIds,
       this.specialPurposeIds,
-      this.specialFeatureIds);
+      this.specialFeatureIds,
+      this.urls);
 
   // When parsing a Vendor, we need to consider that some properties are parsed differently on Android and iOS,
   // mainly because TCFv1 used properties such as purposeIds and TCFv2 uses properties such as purposes.
@@ -38,14 +41,30 @@ class Vendor implements Comparable<Vendor> {
       : id = json["id"],
         name = json["name"],
         policyUrl = json["policyUrl"],
-        namespaces = json["namespaces"]?.cast<String, String>(),
+        namespaces = json["namespaces"]?.cast<Namespaces>(),
         purposeIds = (json["purposeIds"] ?? json["purposes"]).cast<String>(),
         legIntPurposeIds = (json["legIntPurposeIds"] ?? json["legIntPurposes"]).cast<String>(),
         featureIds = (json["featureIds"] ?? json["features"]).cast<String>(),
         flexiblePurposeIds = (json["flexiblePurposeIds"] ?? json["flexiblePurposes"]).cast<String>(),
         specialPurposeIds = (json["specialPurposeIds"] ?? json["specialPurposes"]).cast<String>(),
-        specialFeatureIds = (json["specialFeatureIds"] ?? json["specialFeatures"]).cast<String>();
+        specialFeatureIds = (json["specialFeatureIds"] ?? json["specialFeatures"]).cast<String>(),
+        urls = json["urls"]?.cast<Url>();
 
   @override
   int compareTo(Vendor other) => (id ?? "").compareTo(other.id ?? "");
+}
+
+class Namespaces {
+  String iab2;
+  int num;
+
+  Namespaces(this.iab2, this.num);
+}
+
+class Url {
+  String langId;
+  String privacy;
+  String legIntClaim;
+
+  Url(this.langId, this.privacy, this.legIntClaim);
 }
