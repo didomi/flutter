0dea9b3c3b0698d8b4d4824ab1acf431f20a5d56
Philemon Merlet <philemon.merlet@didomi.io>
2024-12-06 17:27:39 +0100

    Add event parameter

--- lib/events/sync_ready_event.dart
+++ lib/events/sync_ready_event.dart
@@ -1,8 +1,10 @@
 class SyncReadyEvent {
+  // The organization user ID corresponding to the event
+  String organizationUserId;
   // Boolean that indicates whether remote user status has been applied locally.
   bool statusApplied;
   // Function used to send a Sync Acknowledged API Event. Returns **true** if the API Event was sent, **false** otherwise.
   Future<bool> Function() syncAcknowledged = () async => false;
 
-  SyncReadyEvent(this.statusApplied, this.syncAcknowledged);
+  SyncReadyEvent(this.organizationUserId, this.statusApplied, this.syncAcknowledged);
 }
