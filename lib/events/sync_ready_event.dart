8323738df1202a2bda1d56036002769617ddf0d3
Philemon Merlet <philemon.merlet@didomi.io>
2024-12-06 18:42:36 +0100

    Fix failure

--- lib/events/sync_ready_event.dart
+++ lib/events/sync_ready_event.dart
@@ -1,6 +1,6 @@
 class SyncReadyEvent {
   // The organization user ID corresponding to the event
-  String organizationUserId;
+  String? organizationUserId;
   // Boolean that indicates whether remote user status has been applied locally.
   bool statusApplied;
   // Function used to send a Sync Acknowledged API Event. Returns **true** if the API Event was sent, **false** otherwise.
