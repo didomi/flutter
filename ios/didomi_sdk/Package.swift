26ecee3d0508a63c929db2f779b8f9d13a432d6a
Philémon Merlet <philemon.merlet@didomi.io>
2025-12-05 10:22:26 +0100

    Fix minimum iOS version

--- ios/didomi_sdk/Package.swift
+++ ios/didomi_sdk/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "didomi_sdk",
     platforms: [
-        .iOS("13.0")
+        .iOS("10.0")
     ],
     products: [
         .library(name: "didomi-sdk", targets: ["didomi_sdk"])
