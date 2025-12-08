afe3c1ae17d44b7b810a03a5a71be36fecdb5a4c
Philémon Merlet <67579232+pmerlet-at-didomi@users.noreply.github.com>
2025-12-08 11:56:12 +0100

    Add SPM support (#143)
    * Move library files to support SPM
    
    * Fix project for spm
    
    * Update example podfile
    
    * Add didomi_sdk umbrella
    
    * Fix incorrect classes path
    
    * After attempt to clean unnecessary changes
    
    * Add step to build sample using SPM
    
    * Fix mkdir
    
    * Make sure both CocoaPods and SPM are built correctly
    
    * Update native SDK used for SPM
    
    * Small cleanup for sdk files
    
    * Revert unneeded changes
    
    * Fix minimum iOS version

---
+++ ios/didomi_sdk/Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "didomi_sdk",
+    platforms: [
+        .iOS("10.0")
+    ],
+    products: [
+        .library(name: "didomi-sdk", targets: ["didomi_sdk"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.34.0")
+    ],
+    targets: [
+        .target(
+            name: "DidomiSwift",
+            dependencies: [
+                .product(name: "Didomi", package: "didomi-ios-sdk-spm")
+            ],
+            path: "Sources/DidomiSwift",
+            resources: [],
+            linkerSettings: [
+                .linkedFramework("Flutter", .when(platforms: [.iOS]))
+            ]
+        ),
+        .target(
+            name: "didomi_sdk",
+            dependencies: ["DidomiSwift"],
+            path: "Sources/DidomiObjC",
+            resources: [],
+            publicHeadersPath: "include",
+            linkerSettings: [
+                .linkedFramework("Flutter", .when(platforms: [.iOS]))
+            ]
+        )
+    ]
+)
---
+++ ios/didomi_sdk/Sources/DidomiObjC/include/didomi_sdk.h
@@ -0,0 +1,8 @@
+//
+//  didomi_sdk.h
+//  didomi_sdk
+//
+//  Umbrella header for didomi_sdk module
+//
+
+#import "DidomiSdkPlugin.h"
