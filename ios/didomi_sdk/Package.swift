86a9be31eb8a08f3c91ac56ed032c3fb575eacac
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-07-24 15:54:46 +0200

    Fix iOS deployment target in Swift package manifest
    Align Package.swift platform with the podspec and the native Didomi
    SDK requirement (iOS 13.0 instead of 10.0)

--- ios/didomi_sdk/Package.swift
+++ ios/didomi_sdk/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "didomi_sdk",
     platforms: [
-        .iOS("10.0")
+        .iOS("13.0")
     ],
     products: [
         .library(name: "didomi-sdk", targets: ["didomi_sdk"])
