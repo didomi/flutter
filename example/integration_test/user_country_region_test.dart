5c1e71890e38a27b83e6b32186e6e696a8b05fc1
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-09-15 16:23:36 +0200

    Add integration test for userCountryCode/userRegionCode
    Verified passing on Android (Pixel 6a) and iOS (simulator).

---
+++ example/integration_test/user_country_region_test.dart
@@ -0,0 +1,64 @@
+import 'package:didomi_sdk/didomi_sdk.dart';
+import 'package:didomi_sdk/events/event_listener.dart';
+import 'package:didomi_sdk_example/testapps/sample_for_purpose_tests.dart' as app;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'util/initialize_helper.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  final initializeBtnFinder = find.byKey(Key("initializeSmall"));
+
+  bool isError = false;
+  bool isReady = false;
+
+  final listener = EventListener();
+  listener.onError = (String message) {
+    isError = true;
+  };
+  listener.onReady = () {
+    isReady = true;
+  };
+
+  DidomiSdk.addEventListener(listener);
+
+  group("User country and region", () {
+
+    testWidgets("Get user country/region code without initialization", (WidgetTester tester) async {
+      // Start app
+      app.main();
+      await tester.pumpAndSettle();
+
+      var exceptionThrown = false;
+      try {
+        await DidomiSdk.userCountryCode;
+      } on PlatformException {
+        exceptionThrown = true;
+      }
+      assert(exceptionThrown);
+
+      assert(isError == false);
+      assert(isReady == false);
+    });
+
+    testWidgets("Get user country/region code with initialization", (WidgetTester tester) async {
+      // Start app
+      app.main();
+      await tester.pumpAndSettle();
+
+      await InitializeHelper.initializeIfNeeded(tester, initializeBtnFinder, isReady: () => isReady);
+
+      assert(isError == false);
+      assert(isReady == true);
+
+      final countryCode = await DidomiSdk.userCountryCode;
+      final regionCode = await DidomiSdk.userRegionCode;
+      assert(countryCode != null);
+      assert(regionCode != null);
+    });
+  });
+}
