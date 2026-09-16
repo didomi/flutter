7185fb185c2daa33f5b28d89906fb06a1f875b79
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-09-16 09:40:25 +0200

    Restore Package.swift content lost in FlutterFramework dependency commit
    9630ca1 emptied ios/didomi_sdk/Package.swift instead of updating it,
    breaking every SPM-based iOS build with "Package.swift is empty".
    Restore the manifest with the intended fix: declare FlutterFramework
    as a package dependency and reference it from each target instead of
    the old .linkedFramework("Flutter") linker settings.

---
+++ ios/didomi_sdk/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "didomi_sdk",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "didomi-sdk", targets: ["didomi_sdk"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.50.0"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "DidomiSwift",
+            dependencies: [
+                .product(name: "Didomi", package: "didomi-ios-sdk-spm"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
+            path: "Sources/DidomiSwift",
+            resources: []
+        ),
+        .target(
+            name: "didomi_sdk",
+            dependencies: [
+                "DidomiSwift",
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
+            path: "Sources/DidomiObjC",
+            resources: [],
+            publicHeadersPath: "include"
+        )
+    ]
+)
