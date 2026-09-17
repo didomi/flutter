023b98033a6bf39d561199d11d0f78771bac0da3
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-09-17 16:51:51 +0200

    Add FlutterFramework dependency to iOS Package.swift
    Flutter's SPM plugin tooling requires plugins linking against the
    Flutter framework to declare it as an explicit package dependency.

--- ios/didomi_sdk/Package.swift
+++ ios/didomi_sdk/Package.swift
@@ -12,13 +12,15 @@ let package = Package(
         .library(name: "didomi-sdk", targets: ["didomi_sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.50.0")
+        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.50.0"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
     ],
     targets: [
         .target(
             name: "DidomiSwift",
             dependencies: [
-                .product(name: "Didomi", package: "didomi-ios-sdk-spm")
+                .product(name: "Didomi", package: "didomi-ios-sdk-spm"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
             ],
             path: "Sources/DidomiSwift",
             resources: [],
