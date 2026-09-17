398f2866ed93322b0e1c2ede0dda5000c69b4ef3
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-09-17 16:59:32 +0200

    Revert "Add FlutterFramework dependency to iOS Package.swift"
    This reverts commit 023b98033a6bf39d561199d11d0f78771bac0da3.

--- ios/didomi_sdk/Package.swift
+++ ios/didomi_sdk/Package.swift
@@ -12,15 +12,13 @@ let package = Package(
         .library(name: "didomi-sdk", targets: ["didomi_sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.50.0"),
-        .package(name: "FlutterFramework", path: "../FlutterFramework")
+        .package(url: "https://github.com/didomi/didomi-ios-sdk-spm", from: "2.50.0")
     ],
     targets: [
         .target(
             name: "DidomiSwift",
             dependencies: [
-                .product(name: "Didomi", package: "didomi-ios-sdk-spm"),
-                .product(name: "FlutterFramework", package: "FlutterFramework")
+                .product(name: "Didomi", package: "didomi-ios-sdk-spm")
             ],
             path: "Sources/DidomiSwift",
             resources: [],
